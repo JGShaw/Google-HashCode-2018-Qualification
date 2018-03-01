5885a2b2cd421a27110434ec23a07263c5b6d59b
Joseph Shaw <j.g.shaw95@gmail.com>
2018-03-01 18:14:27 +0000

    Add known variables

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,7 @@
 class Car
   def initialize
-
+    @x = 0
+    @y = 0
+    @rides = []
   end
 end
--- lib/ride.rb
+++ lib/ride.rb
@@ -1,5 +1,13 @@
 class Ride
-  def initialize
+  attr_reader :start_x, :start_y, :end_x, :end_y, :start_time, :end_time, :distance
 
+  def initialize(start_x, start_y, end_x, end_y, start_time, end_time)
+    @start_x = start_x
+    @start_y = start_y
+    @end_x = end_x
+    @end_y = end_y
+    @start_time = start_time
+    @end_time = end_time
+    @distance = (start_x - end_x).abs + (start_y - end_y).abs
   end
 end
