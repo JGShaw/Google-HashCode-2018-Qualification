c59309362548814b335e6ae9e04e4e6d84b5bbfd
Shaun Mendham <sxm1041@users.noreply.github.com>
2018-03-01 18:57:58 +0000

    output file

--- lib/main.rb
+++ lib/main.rb
@@ -15,10 +15,29 @@ File.open(ARGV[0], "r") do |f|
         ride_id += 1
     end
 
-    cars = []
+    @cars = []
     
     for i in 0..@numVehicles
-        cars << Car.new
+        @cars << Car.new
     end
 
+    @cars[0].rides << rides[0]
+    @cars[0].rides << rides[1]
+    @cars[0].rides << rides[2]
+    @cars[1].rides << rides[1]
+
 end
+
+fileName = File.basename(ARGV[0], File.extname(ARGV[0]))
+
+File.open("outputs/%s.out" % [fileName], 'w') { |file| 
+    @cars.each do |car|
+        output_line = car.rides.map{ |ride| ride.ride_id }.join(" ")
+        file.puts output_line unless output_line.empty?
+    end
+}
+
+
+
+    
+
