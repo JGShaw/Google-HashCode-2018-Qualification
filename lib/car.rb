2b381af1bae851865da9e1c77fbec047e77352c8
Joseph Shaw <j.g.shaw95@gmail.com>
2018-03-01 20:37:37 +0000

    temp

--- lib/car.rb
+++ lib/car.rb
@@ -9,20 +9,40 @@ class Car
   end
 
   def time_for_ride(ride)
-    @total_time + time_to_ride_complete(ride)
+    min_time = nil
+    (@rides.length + 1).times do |index| 
+      all_rides = @rides.clone
+      all_rides.insert(index, ride) 
+      time = time_to_finish_all_rides(all_rides)
+      
+      if min_time.nil? || time < min_time
+        min_time = time
+        @min_bid = all_rides
+      end
+    end
+    min_time 
   end
 
   def add_ride(ride)
-    @x = ride.end_x
-    @y = ride.end_y
-    @total_time += time_to_ride_complete(ride) 
-    @rides << ride
+    @rides = @min_bid
   end
 
   private
 
-  def time_to_ride_complete(ride)
-    (@x - ride.start_x).abs + (@y - ride.start_y).abs + ride.distance
+  def time_to_ride_complete(x, y, ride)
+    (x - ride.start_x).abs + (y - ride.start_y).abs + ride.distance
+  end
+
+  def time_to_finish_all_rides(rides)
+    total_time = 0
+    x = 0
+    y = 0
+    rides.each do |ride|
+      total_time += time_to_ride_complete(x, y, ride)
+      x = ride.end_x
+      y = ride.end_y
+    end
+    total_time
   end
 
 end
--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
+echo "Starting a"
 ruby lib/main.rb inputs/a_example.in
+echo "Starting b"
 ruby lib/main.rb inputs/b_should_be_easy.in
+echo "Starting c"
 ruby lib/main.rb inputs/c_no_hurry.in
+echo "Starting d"
 ruby lib/main.rb inputs/d_metropolis.in
+echo "Starting e"
 ruby lib/main.rb inputs/e_high_bonus.in
 rm code.zip
 zip -r code.zip *
