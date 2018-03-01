6c4ac93712dd8fc51fbf3f7095d4ab149baf217e
Joseph Shaw <j.g.shaw95@gmail.com>
2018-03-01 19:17:16 +0000

    remove current zip file before zipping

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ ruby lib/main.rb inputs/b_should_be_easy.in
 ruby lib/main.rb inputs/c_no_hurry.in
 ruby lib/main.rb inputs/d_metropolis.in
 ruby lib/main.rb inputs/e_high_bonus.in
+rm code.zip
 zip -r code.zip *
