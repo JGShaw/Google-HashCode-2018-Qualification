892f93b87f10f89c7d7274f70b5327b62953ca2c
Joseph Shaw <j.g.shaw95@gmail.com>
2018-03-02 10:42:34 +0000

    tidy up

--- lib/car.rb
+++ lib/car.rb
@@ -13,27 +13,20 @@ class Car
   def score_with_ride(ride)
     score = @score
     time = @time
-
     time += (@x - ride.start_x).abs + (@y - ride.start_y).abs
-
     wait_time = ride.start_time - time
     time += wait_time if wait_time > 0
-
     score += @bonus if time == ride.start_time
     time += ride.distance
     score += ride.distance if time <= ride.end_time
-
     score
   end
 
   def add_ride(ride)
     @rides << ride
     @time += (@x - ride.start_x).abs + (@y - ride.start_y).abs
-
-
     wait_time = ride.start_time - @time
     @time += wait_time if wait_time > 0
-
     @score += @bonus if @time == ride.start_time
     @time += ride.distance
     @score += ride.distance if (@time <= ride.end_time) 
